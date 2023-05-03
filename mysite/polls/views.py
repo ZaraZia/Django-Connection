b2e2a21812bacea7dd9ed4ff892da14bcaddbb18
ZaraZia <zarazia01@gmail.com>
2023-05-03 20:08:40 +0500

    changing in branch 2 -> 2x

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,3 @@
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("text for branch 2.")
+    return HttpResponse("text for branch 2, changing for second time to check.")
